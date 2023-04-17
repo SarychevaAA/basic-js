ce9d2990139b4a9db5dc538ab6372ee4f16c5fe2
Aleksandra <alex.sarychewa2014@yandex.ru>
2023-04-17 19:32:05 +0300

    feat: add solution for some tasks

--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -13,10 +13,16 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  */
 class DepthCalculator {
-  calculateDepth(/* arr */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  calculateDepth(arr){
+    let maxDepth = 1
+    for (let element of arr){
+      if (Array.isArray(element)){
+        maxDepth = Math.max(this.calculateDepth(element)+1, maxDepth)
+      }
+    }
+    return maxDepth;
   }
+
 }
 
 module.exports = {
--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -20,15 +20,51 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor(type = true) {
+    this.type = type
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  encrypt(message, key) {
+    if (!message || !key) throw new Error('Incorrect arguments!');
+    message = message.toUpperCase();
+    key = key.toUpperCase();
+    let keyStr = key.repeat(Math.ceil(message.length/key.length)).slice(0, message.length)
+    let result = '';
+    let j = 0
+    for (let i=0; i<message.length; i++){
+      if (message[i].match(/[A-Z]/g)){
+        console.log("E: ", message[i])
+        result += String.fromCharCode(((message.charCodeAt(i)+keyStr.charCodeAt(j))%26+'A'.charCodeAt(0)))
+
+        j += 1
+      }
+      else{
+        result += message[i];
+      }
+    }
+    return (this.type)? result: result.split("").reverse().join('') ;
+
+  }
+  decrypt(encryptedMessage, key) {
+    if (!encryptedMessage || !key) throw new Error('Incorrect arguments!');
+    key = key.toUpperCase();
+    let keyStr = key.repeat(Math.ceil(encryptedMessage.length/key.length)).slice(0, encryptedMessage.length)
+    let result = '';
+    let j = 0
+    for (let i=0; i<encryptedMessage.length; i++){
+      if (encryptedMessage[i].match(/[A-Z]/g)){
+        console.log("E: ", encryptedMessage[i])
+        result += String.fromCharCode(((encryptedMessage.charCodeAt(i)-keyStr.charCodeAt(j)+26)%26+'A'.charCodeAt(0)))
+        j += 1
+      }
+      else{
+        result += encryptedMessage[i];
+      }
+    }
+    return (this.type)? result: result.split("").reverse().join('') ;
+
   }
 }
+
 
 module.exports = {
   VigenereCipheringMachine
